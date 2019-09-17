b67e09d8b5c4916bca0505d0dfb061c0ca269fc4
huning <304873772@qq.com>
2019-09-17 11:08:24 +0800

    feat: 5

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import '@assets/index.less';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log(5)
 ReactDOM.render(<App />, document.getElementById('root'));
 console.log(333)
 // If you want your app to work offline and load faster, you can change
